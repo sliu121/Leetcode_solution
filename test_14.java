e3fcdd6f9f87e6cd1ad37cab7306ff4b7b393c80
Unknown <lsy930611@hotmail.com>
2019-03-01 15:12:34 -0500

    Q14

---
+++ test_14.java
@@ -0,0 +1,29 @@
+package test_14;
+
+public class test_14 {
+
+    public static void main(String[] args){
+        String[] strs = {"flower","flow","flight"};
+        String test = longestCommonPrefix(strs);
+        System.out.println(test);
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        if(strs.length == 0){
+            return "";
+        }
+
+        String str = strs[0];
+
+        for(int i = 1; i < strs.length; i++ ){
+            while(strs[i].indexOf(str) != 0){
+                str = str.substring(0,str.length()-1);
+            }
+            if(str.isEmpty()){
+                return "";
+            }
+        }
+        return str;
+    }
+
+}
